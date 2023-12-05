73c35d5f12799171ddcda370965e73c0f6fc6286
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-12-05 21:58:40 +0100

    Personalize navbar

--- lib/widget/bottom_navbar.dart
+++ lib/widget/bottom_navbar.dart
@@ -4,26 +4,44 @@ class BottomNavbar extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      type: BottomNavigationBarType.fixed,
-      items: const <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Accueil',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.business),
-          label: 'Business',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.access_time_filled_rounded),
-          label: 'Encee',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.add_circle_outline), // Ajoutez l'icône de votre choix
-          label: 'Nouveau', // Ajoutez le libellé de votre choix
-        ),
-      ],
+    return Container (
+      margin: const EdgeInsets.only(right: 5, left: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+        color: const Color(0xFFE9E9E9),
+      ),
+      child: BottomNavigationBar(
+        backgroundColor: Colors.transparent,
+
+          type: BottomNavigationBarType.fixed, //Must add this if icones>3
+          elevation: 0, //Hide shadow
+          iconSize: 30,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          selectedItemColor: Color(0xFF222222),
+          unselectedItemColor: Colors.black38,
+
+          items: const <BottomNavigationBarItem>[
+
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_box_outlined),
+              label: 'Add product',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_card_outlined),
+              label: 'Chat',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle_outlined),
+              label: 'Profil',
+            ),
+          ]
+
+    ),
     );
   }
 
