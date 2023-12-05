9af5460a5e821904291bb7c9eecc03c2366c356d
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-12-05 20:30:38 +0100

    Add description and image placement in product_card

--- lib/widget/product_card.dart
+++ lib/widget/product_card.dart
@@ -15,13 +15,51 @@ class _ProductCard extends State<ProductCard> {
         double width = constraints.maxWidth * 0.45;
         double marginPerfect = constraints.maxWidth * 0.03333;
         return Container(
-          width: width,
-          height: width + 50,
-          color: Colors.red,
-          margin: EdgeInsets.only(left: marginPerfect, top: marginPerfect),
+            width: width,
+            height: width + 70,
+            color: Colors.red,
+            margin: EdgeInsets.only(left: marginPerfect, top: marginPerfect),
+            child: Column(
+              children:<Widget> [
+                Container(
+                  width: double.infinity,
+                  height: width,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(25),
+                      color: Colors.amberAccent
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 30,
+                  color: Colors.brown,
+                  margin: const EdgeInsets.only(top: 5),
+                  child: const Text(
+                    "test de",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  height: 30,
+                  color: Colors.brown,
+                  margin: const EdgeInsets.only(top: 5),
+                  child: const Text(
+                    "50E",
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
+            )
         );
       },
     );
   }
+
 
 }
