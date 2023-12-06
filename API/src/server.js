6dd45c2daefeeefe62d0fe89219412347bcf9df8
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-12-06 23:44:05 +0100

    Try to connect flutter with backend

--- API/src/server.js
+++ API/src/server.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const db = require('./database/database');
 const cors = require('cors');
-const categoryController = require('./controllers/category_controller')
+const categoryController = require('./controllers/category_controller');
+const path = require('path');
+
 
 app.use(bodyParser.json());
 const http = require('http');
@@ -16,6 +18,7 @@ app.use(cors(
     }
 ));
 
+app.use('/assets', express.static(path.join(__dirname, '../assets')));
 app.use('/api/category', categoryController);
 
 server.listen(3000, () => {
