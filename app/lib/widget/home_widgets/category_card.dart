b292029014c95a7bf9079c0e55fd30bc502ebfdb
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-12-07 22:50:37 +0100

    Category home almost done, add filter on image

--- app/lib/widget/home_widgets/category_card.dart
+++ app/lib/widget/home_widgets/category_card.dart
@@ -19,14 +19,41 @@ class _CategoryCard extends State<CategoryCard> {
       width: 150.0,
       height: 100.0,
       margin: const EdgeInsets.only(left: 15),
-      decoration: BoxDecoration(
-        color: Color(0xFFE9E9E9),
-        borderRadius: BorderRadius.circular(10.0),
-        image: DecorationImage(
-          image: Image.network('${baseUrl}assets/static/category_img/${widget.category.image}').image,
-          fit: BoxFit.cover,
-        ),
+      child: Stack(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: Color(0xFFE9E9E9),
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage('${baseUrl}assets/static/category_img/${widget.category.image}'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(
+                  Colors.black.withOpacity(0.3),
+                  BlendMode.darken,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                widget.category.category_name,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 16.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
