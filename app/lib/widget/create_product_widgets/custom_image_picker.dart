41203132f5a4b7126dc81c6808e823f9a1d6be86
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-12-08 00:27:15 +0100

    Add remove picture

--- app/lib/widget/create_product_widgets/custom_image_picker.dart
+++ app/lib/widget/create_product_widgets/custom_image_picker.dart
@@ -17,11 +17,17 @@ class _CustomImagePickerState extends State<CustomImagePicker> {
   Future<void> selectImage() async {
     final XFile? image = await _picker.pickImage(source: ImageSource.gallery);
 
-    if (image != null) {
+    if (image != null && images.length < 3) {
       setState(() {
         images.add(image);
       });
     }
+  }
+
+  void removeImage(int index) {
+    setState(() {
+      images.removeAt(index);
+    });
   }
 
   @override
@@ -32,33 +38,53 @@ class _CustomImagePickerState extends State<CustomImagePicker> {
       margin: const EdgeInsets.only(top: 20, left: 15, right: 15),
       child: Stack(
         children: [
+
           Align(
             alignment: Alignment.centerLeft,
+
             child: GestureDetector(
               onTap: selectImage,
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.60,
-                height: double.infinity,
-                margin: const EdgeInsets.only(right: 10),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(
-                    color: Colors.grey,
-                    width: 2,
+
+              child: Stack(
+                children: [
+
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.60,
+                    height: double.infinity,
+                    margin: const EdgeInsets.only(right: 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(5),
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2,
+                      ),
+                      image: images.isNotEmpty ? DecorationImage(
+                        image: FileImage(File(images[0].path)),
+                        fit: BoxFit.cover,
+                      ) : null,
+                    ),
+
+                    child: images.isNotEmpty ? null : Center(
+                      child: Text('+', style: TextStyle(
+                          fontSize: 70,
+                          color: Colors.grey.withOpacity(0.5)
+                      )),
+                    ),
                   ),
-                  image: images.isNotEmpty ? DecorationImage(
-                    image: FileImage(File(images[0].path)),
-                    fit: BoxFit.cover,
-                  ) : null,
-                ),
-                child: images.isNotEmpty ? null : Center(
-                  child: Text('+', style: TextStyle(
-                      fontSize: 70,
-                      color: Colors.grey.withOpacity(0.5)
-                  )),
-                ),
+
+                  if (images.isNotEmpty)
+                    Container(
+                        color: Colors.black.withOpacity(0.5),
+                        child: IconButton(
+                          icon: const Icon(Icons.delete, color: Colors.white),
+                          onPressed: () => removeImage(0),
+                        ),
+                    ),
+                ],
               ),
+
             ),
+
           ),
 
           Align(
@@ -68,53 +94,85 @@ class _CustomImagePickerState extends State<CustomImagePicker> {
               height: double.infinity,
               child: Column(
                 children: [
+
                   GestureDetector(
                     onTap: selectImage,
-                    child: Container(
-                      width: double.infinity,
-                      height: 95,
-                      margin: EdgeInsets.only(bottom: 10),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2,
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 95,
+                          margin: EdgeInsets.only(bottom: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 2,
+                            ),
+                            image: images.length > 1 ? DecorationImage(
+                              image: FileImage(File(images[1].path)),
+                              fit: BoxFit.cover,
+                            ) : null,
+                          ),
+                          child: images.length > 1 ? null : Center(
+                            child: Text('+', style: TextStyle(
+                                fontSize: 70,
+                                color: Colors.grey.withOpacity(0.5)
+                            )),
+                          ),
                         ),
-                        image: images.length > 1 ? DecorationImage(
-                          image: FileImage(File(images[1].path)),
-                          fit: BoxFit.cover,
-                        ) : null,
-                      ),
-                      child: images.length > 1 ? null : Center(
-                        child: Text('+', style: TextStyle(
-                            fontSize: 70,
-                            color: Colors.grey.withOpacity(0.5)
-                        )),
-                      ),
+                        if (images.length > 1)
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              color: Colors.black.withOpacity(0.5),
+                              child: IconButton(
+                                icon: const Icon(Icons.delete, color: Colors.white),
+                                onPressed: () => removeImage(1),
+                              ),
+                            ),
+                          ),
+                      ],
                     ),
                   ),
+
                   GestureDetector(
                     onTap: selectImage,
-                    child: Container(
-                      width: double.infinity,
-                      height: 95,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2,
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: double.infinity,
+                          height: 95,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            border: Border.all(
+                              color: Colors.grey,
+                              width: 2,
+                            ),
+                            image: images.length > 2 ? DecorationImage(
+                              image: FileImage(File(images[2].path)),
+                              fit: BoxFit.cover,
+                            ) : null,
+                          ),
+                          child: images.length > 2 ? null : Center(
+                            child: Text('+', style: TextStyle(
+                                fontSize: 70,
+                                color: Colors.grey.withOpacity(0.5)
+                            )),
+                          ),
                         ),
-                        image: images.length > 2 ? DecorationImage(
-                          image: FileImage(File(images[2].path)),
-                          fit: BoxFit.cover,
-                        ) : null,
-                      ),
-                      child: images.length > 2 ? null : Center(
-                        child: Text('+', style: TextStyle(
-                            fontSize: 70,
-                            color: Colors.grey.withOpacity(0.5)
-                        )),
-                      ),
+                        if (images.length > 2)
+                          Align(
+                            alignment: Alignment.topRight,
+                            child: Container(
+                              color: Colors.black.withOpacity(0.5),
+                              child: IconButton(
+                                icon: const Icon(Icons.delete, color: Colors.white),
+                                onPressed: () => removeImage(2),
+                              ),
+                            ),
+                          ),
+                      ],
                     ),
                   ),
                 ],
