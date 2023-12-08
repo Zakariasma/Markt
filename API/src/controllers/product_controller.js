e44b22ed02d14e239d8b203b626696478f0446f0
Zakaria Smaali <101876655+Zakariasma@users.noreply.github.com>
2023-12-08 17:10:29 +0100

    Can send pictureList in API

--- API/src/controllers/product_controller.js
+++ API/src/controllers/product_controller.js
@@ -2,15 +2,32 @@ const express = require('express');
 const ProductService = require('../services/product_service');
 const router = express.Router();
 
+const multer = require('multer');
+const path = require('path');
 
-router.post('/', async (req, res) => {
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, '../assets/static/product_img');
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+router.post('/', upload.array('pictureList', 3), async (req, res) => {
     try {
-        const product = await ProductService.createProduct(req.body)
+        let pictureList = req.files.map(file => file.filename);
+        req.body.pictureList = pictureList;
+        const product = await ProductService.createProduct(req.body);
         res.json(product);
     } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
+
+
 
 
 
